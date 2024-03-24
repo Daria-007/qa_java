b923785adf6b13c526b0676bc96f8ae4de49f821
Daria Ermolaeva <ermolaeva.daria007@yandex.ru>
2024-03-24 22:05:35 +0300

    add tests

--- src/main/java/com/example/Predator.java
+++ src/main/java/com/example/Predator.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.List;
 
 public interface Predator {
-
+    int getKittens();
+    List<String> getFood(String animalKind) throws Exception;
     List<String> eatMeat() throws Exception;
-
 }
